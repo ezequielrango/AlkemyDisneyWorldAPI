991f1be525624f069442ece12381a5639f8cf36f
unknown <ezequielrango@hotmail.com>
2021-11-23 19:27:05 -0300

    create middleware upload charactersImage

---
+++ middlewares/UploadImageCharacter.js
@@ -0,0 +1,18 @@
+const multer = require('multer');
+const path = require('path')
+
+const storage =  multer.diskStorage({
+
+    destination : (req,file,cb) => {
+        cb(null,path.join(__dirname,'../images/characters'))
+    },
+
+    filename : (req,file,cb) => {
+        const newFileName = `img-${Date.now()}${path.extname(file.originalname)}`;
+        cb(null,newFileName);
+    }
+});
+
+const upload= multer({storage : storage});
+
+module.exports  = upload;
